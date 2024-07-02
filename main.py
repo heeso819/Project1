0b2fec512ee6e2d7acf24a116f86663fb130216d
Hong Kil Dong <johnbann@hanafos.com>
2024-07-02 15:48:35 +0900

    Revert "네 번째 커밋"
    This reverts commit aa8a1e79a9e9eb93304437a35015f2f2c98e0daf.

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ def mul_func(n1,n2) :
     retVal = n1 * n2
     return retVal
 
-def div_func(n1,n2) :
-    retVal = n1 / n2
-    return retVal
-
 
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
@@ -29,6 +25,3 @@ print(num1,'-',num2,'=',res)
 
 res = mul_func(num1, num2)
 print(num1,'*',num2,'=',res)
-
-res = div_func(num1, num2)
-print(num1,'/',num2,'=',res)
